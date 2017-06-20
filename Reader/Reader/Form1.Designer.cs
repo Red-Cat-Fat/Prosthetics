1e401a8ab6660a7726d340cfb95934cfbe45bcd3
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2017-06-20 16:24:24 +0300

    Что-то считывает

--- Reader/Reader/Form1.Designer.cs
+++ Reader/Reader/Form1.Designer.cs
@@ -32,6 +32,13 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.write_button = new System.Windows.Forms.Button();
             this.read_button = new System.Windows.Forms.Button();
+            this.data_textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.idPort_textBox = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -47,12 +54,12 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(47, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.Size = new System.Drawing.Size(202, 20);
             this.textBox1.TabIndex = 1;
             // 
             // write_button
             // 
-            this.write_button.Location = new System.Drawing.Point(12, 32);
+            this.write_button.Location = new System.Drawing.Point(12, 233);
             this.write_button.Name = "write_button";
             this.write_button.Size = new System.Drawing.Size(75, 23);
             this.write_button.TabIndex = 2;
@@ -62,7 +69,7 @@
             // 
             // read_button
             // 
-            this.read_button.Location = new System.Drawing.Point(153, 32);
+            this.read_button.Location = new System.Drawing.Point(174, 233);
             this.read_button.Name = "read_button";
             this.read_button.Size = new System.Drawing.Size(75, 23);
             this.read_button.TabIndex = 3;
@@ -70,17 +77,74 @@
             this.read_button.UseVisualStyleBackColor = true;
             this.read_button.Click += new System.EventHandler(this.read_button_Click);
             // 
+            // data_textBox
+            // 
+            this.data_textBox.Location = new System.Drawing.Point(12, 74);
+            this.data_textBox.Multiline = true;
+            this.data_textBox.Name = "data_textBox";
+            this.data_textBox.Size = new System.Drawing.Size(237, 153);
+            this.data_textBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Data";
+            // 
+            // connect_button
+            // 
+            this.connect_button.Location = new System.Drawing.Point(15, 32);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(89, 23);
+            this.connect_button.TabIndex = 6;
+            this.connect_button.Text = "Подключение";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
+            // 
+            // idPort_textBox
+            // 
+            this.idPort_textBox.Location = new System.Drawing.Point(110, 34);
+            this.idPort_textBox.Name = "idPort_textBox";
+            this.idPort_textBox.Size = new System.Drawing.Size(138, 20);
+            this.idPort_textBox.TabIndex = 7;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 264);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(255, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(10, 17);
+            this.statusLabel.Text = " ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(246, 64);
+            this.ClientSize = new System.Drawing.Size(255, 286);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.idPort_textBox);
+            this.Controls.Add(this.connect_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.data_textBox);
             this.Controls.Add(this.read_button);
             this.Controls.Add(this.write_button);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,6 +156,12 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button write_button;
         private System.Windows.Forms.Button read_button;
+        private System.Windows.Forms.TextBox data_textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.TextBox idPort_textBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
     }
 }
 
--- Reader/Reader/Form1.cs
+++ Reader/Reader/Form1.cs
@@ -21,9 +21,12 @@ namespace Reader
         private void write_button_Click(object sender, EventArgs e)
         {
             createFile(textBox1.Text);
-            writeFile("ItsWork!!!");
+            //writeFile("ItsWork!!!");
+            //closeFiles();
+            statusLabel.Text = "Идёт запись";
+            writeSerial(s);
             closeFiles();
-            write_button.Text = "END";
+            statusLabel.Text = "Записанно";
         }
 
         private void read_button_Click(object sender, EventArgs e)
@@ -44,9 +47,8 @@ namespace Reader
         void createFile(string nameFile)
         {
             DateTime thisDay = DateTime.Today;
-            string path = @"dataBase-" + thisDay.ToString("d");// +'\\' + nameFile + ".mio";
-            string file = path + '-' + nameFile + ".mio";
-            fstream = new FileStream(file, FileMode.OpenOrCreate);
+            string way = @"dataBase-" + thisDay.ToString("d") + '-' + thisDay.Hour + '.' + thisDay.Minute + '-' + nameFile + ".mio";
+            fstream = new FileStream(way, FileMode.OpenOrCreate);
         }
 
         void openFile(string nameFile)
@@ -69,6 +71,53 @@ namespace Reader
         void closeFiles()
         {
             fstream.Close();
+        }
+        Serial s = new Serial();
+        private void connect_button_Click(object sender, EventArgs e)
+        {
+            if (idPort_textBox.Text == "")
+            {
+                s.UpdatePorts();
+
+                for (int i = 0; i < s.AvailablePorts.Length; i++)
+                {
+                    data_textBox.Text = '[' + Convert.ToString(i) + ']';
+                    data_textBox.Text += s.AvailablePorts[i];
+                }
+                statusLabel.Text = "Выберите порт";
+            }
+            else
+            {
+                s.UpdatePorts();
+                int portID = Convert.ToInt32(idPort_textBox.Text);
+
+                s.Create(portID, 115200);
+
+                s.Open();
+                statusLabel.Text = "Подключенно";
+                /*MainForm mf = new MainForm();
+                mf.Show();*/
+                //int z = 0;
+                //while (true)
+                //{
+                //    if (++z == 100000)
+                //    {
+                //        data_textBox.Text =  + '\n';
+                //        z = 0;
+                //    }
+                //}
+            }
+        }
+
+        void writeSerial(Serial serial)
+        {
+            int j = 0;
+            while (j < 10000)
+            {
+                writeFile(serial.WaitRecv());
+                statusLabel.Text = serial.WaitRecv();
+                j++;
+            }
         }
     }
 }
